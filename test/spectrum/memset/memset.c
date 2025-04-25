e7c069fb1620c15b96b27b6841ab52048919b186
Derek Fountain <derek@scratters.com>
2025-04-25 15:25:33 +0100

    more tweaking with the memset testcase

--- test/spectrum/memset/memset.c
+++ test/spectrum/memset/memset.c
@@ -1,5 +1,5 @@
 /*
- * zcc +zx -vn -startup=1 -clib=sdcc_iy memset.c -o z80_image
+ * zcc +zx -vn -startup=0 -clib=sdcc_iy memset.c -o z80_image
  * xxd -i -c 16 z80_image_CODE.bin > ../../../firmware/z80_image.h
  */
 
@@ -12,12 +12,13 @@
 
 void main(void)
 {
-  uint8_t memset_cmd[] =
+  /* Compiler gives internal error if this isn't static */
+  static uint8_t memset_cmd[] =
   {
     128, 0, 0,                 // CMD type, result and error
 
-    0, 0xc0,                   // zx_addr to set memory at C000
-    0x55,                      // c, constant value to set
+    0x00, 0x00,                // zx_addr to set memory at
+    0x00,                      // c, constant value to set
     0, 0,                      // n, 16 bit count to set
   };
 
@@ -28,10 +29,16 @@ void main(void)
   srand( 1000 );
   while(1)
   {
-    uint8_t length = (rand() & 0x1F)+1;
+    uint8_t  length = (rand() & 0x1F)+1;
+
+//    uint16_t dest_addr = 0x5B00;            // Lower RAM, contended, printer buffer
+    uint16_t dest_addr = 0xC000;            // Upper RAM, not contended, above code, below stack
 
     memset_cmd[1] = 0;    // Response
     memset_cmd[2] = 0;    // Error
+
+    memset_cmd[3] = dest_addr & 0xFF;
+    memset_cmd[4] = (dest_addr>>8) & 0xFF;
 
     memset_cmd[5] = c_value;
 
@@ -39,6 +46,14 @@ void main(void)
     memset_cmd[7] = 0;
 
     uint16_t memset_cmd_addr = (uint16_t)(&memset_cmd[0]);
+
+    printf("Check %ld, %02X times %d bytes?\n", check_counter, c_value, length);
+
+    printf("%02X %02X %02X  %02X %02X  %02X  %02X %02X\n",
+	   memset_cmd[0],memset_cmd[1],memset_cmd[2],
+	   memset_cmd[3],memset_cmd[4],
+	   memset_cmd[5],
+	   memset_cmd[6],memset_cmd[7]);
 
     *((uint8_t*)14446) = (uint8_t)(memset_cmd_addr & 0xFF);
     *((uint8_t*)14447) = (uint8_t)((memset_cmd_addr >> 8) & 0xFF);
@@ -60,26 +75,20 @@ void main(void)
 	if( memset_cmd[2] != 0 )
 	{
 	  printf("Error is %d\n", memset_cmd[2]);
-	  printf("From %02X %02X %02X   %02X %02X   %02X   %02X %02X\n",
-		 memset_cmd[0],memset_cmd[1],memset_cmd[2],
-		 memset_cmd[3],memset_cmd[4],
-		 memset_cmd[5],
-		 memset_cmd[6],memset_cmd[7]);
 	  while(1);
 	}
+
       }
     }
-
-    printf("Check %ld, %02X times %d bytes?\n", check_counter, c_value, length);
 
     uint16_t counter;
     for( counter=0; counter < length; counter++ )
     {
-      uint8_t *check_addr = (uint8_t*)(0xc000+counter);
+      uint8_t *check_addr = (uint8_t*)(dest_addr+counter);
       if( *check_addr != c_value )
       {
-	printf("Fail at %ld, expected 0x55, found %02X\n",
-	       check_addr, *check_addr);
+	printf("Fail at %p, expected %02X, found %02X\n",
+	       check_addr, c_value, *check_addr);
 	while(1);
       }
     }
